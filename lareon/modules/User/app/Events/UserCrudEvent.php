1d10cab729567024e2a6ed1ef13dd7dc52fb8f0c
teksite <teksite.design@gmail.com>
2026-05-24 21:04:35 +0330

    meow

---
+++ lareon/modules/User/app/Events/UserCrudEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Lareon\Modules\User\App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Lareon\Modules\User\App\Models\User;
+
+class UserCrudEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public User|Authenticatable $user, public string $operation, public array $data = [])
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
