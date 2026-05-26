a58a87b745a95cc5f19f082b445789c39b8c46d6
teksite <teksite.design@gmail.com>
2026-05-26 21:53:49 +0330

    fix issue in menu in header

--- lareon/steward/resources/views/admin/layouts/partials/header.blade.php
+++ lareon/steward/resources/views/admin/layouts/partials/header.blade.php
@@ -1,5 +1,5 @@
-<header class="mb-6 w-full rounded-lg overflow-hidden  bg-theme-4 bg-cover bg-no-repeat bg-fixed">
-    <div class="bg-linear-to-r from-cyan-500 to-blue-500 50 w-full h-full ">
+<header class="mb-6 w-full rounded-lg bg-theme-4 bg-cover bg-no-repeat bg-fixed">
+    <div class="bg-linear-to-r from-cyan-500 to-blue-500 50 w-full h-full relative rounded-xl">
         <div class="w-full h-full p-3 min-h-64 flex items-center">
             <div class="px-3">
                 <h2 class="mb-3 text-zinc-50">
@@ -11,7 +11,7 @@
             </div>
         </div>
         @if(View::hasSection('header.start') || View::hasSection('header.end') )
-            <div class="x-box flex flex-col sm:flex-row items-center justify-between gap-6 -translate-y-12 -mt-12 w-11/12 mx-auto">
+            <div class="absolute x-box flex flex-col sm:flex-row items-center justify-between gap-6 -translate-y-12 inset-x-0 w-11/12 mx-auto">
                 <div class="flex items-center justify-start gap-3">
                     @yield('header.start')
                 </div>
