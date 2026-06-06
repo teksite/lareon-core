15ad040c1cc1769c77329bbd008cfce2f3779c47
teksite <teksite.design@gmail.com>
2026-06-06 13:36:06 +0330

    make otp view section

--- lareon/modules/Auth/resources/views/authentication/pages/partials/otp.blade.php
+++ lareon/modules/Auth/resources/views/authentication/pages/partials/otp.blade.php
@@ -1,4 +1,4 @@
-<div x-show="activeTab === 1" x-transition.opacity.duration.300ms>
+<div x-show="activeTab === 3" x-transition.opacity.duration.300ms>
     <section>
         <form method="POST" action="{{route('auth.otp.login.store')}}" class="formAction">
             @csrf
@@ -6,10 +6,22 @@
                 <x-lareon::inputs.label for="code" :title="__('enter the code from your authentication app')" class=""/>
                 <div class="flex items-center justify-center gap-1 mt-3" dir="ltr">
                     @for($i=0 ;$i<6 ; $i++)
-                        <input class="otpField oneFieldInput border border-zinc-300 px-4 py-3 w-12 h-12 font-bold {{$i==2 ? 'me-3' : ''}} {{$i==3 ? 'ms-3': ''}}" name="otp_code[]" type="text"  maxlength=1 >
+                        <input class="otpField oneFieldInput border border-zinc-300 px-4 py-3 w-12 h-12 font-bold {{$i==2 ? 'me-3' : ''}} {{$i==3 ? 'ms-3': ''}}" name="otp_code[]" type="text" maxlength=1>
                     @endfor
                 </div>
                 <x-lareon::inputs.error :messages="$errors->get('otp_code')" class="mt-2"/>
+            </div>
+            <div class="flex items-center my-12">
+                @if(true)
+                    <x-lareon::buttons.simple color="blue" variant="simple" size="xs" role="button" type="button">
+                        {{__('send via email')}}
+                    </x-lareon::buttons.simple>
+                @endif
+                @if(true)
+                    <x-lareon::buttons.simple color="blue" variant="simple" size="xs" role="button" type="button">
+                        {{__('send via SMS')}}
+                    </x-lareon::buttons.simple>
+                @endif
             </div>
             <div class="">
                 <x-lareon::buttons.simple id="totpFieldSubmit" type="submit" rol="button" :fullWidth="true" title="{{ __('confirm') }}">
--- lareon/steward/resources/views/components/buttons/simple.blade.php
+++ lareon/steward/resources/views/components/buttons/simple.blade.php
@@ -14,34 +14,42 @@
         'blue' => [
             'solid' => 'shadow-lg shadow-blue-600/50 bg-blue-600 hover:bg-blue-700 active:bg-blue-900 text-white hover:shadow-sm focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
             'outline' => 'border-2 border-blue-600 text-blue-600 hover:bg-blue-50 active:bg-blue-100 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
+            'simple' => 'text-blue-600 hover:bg-blue-50 active:bg-blue-100',
         ],
         'gray' => [
             'solid' => 'shadow-lg shadow-gray-600/50 bg-gray-600 hover:bg-gray-700 active:bg-gray-900 text-white hover:shadow-sm focus:ring-2 focus:ring-gray-500 focus:ring-offset-2',
             'outline' => 'border-2 border-gray-600 text-gray-600 hover:bg-gray-50 active:bg-gray-100 focus:ring-2 focus:ring-gray-500 focus:ring-offset-2',
+            'simple' => 'text-gray-600 hover:bg-gray-50 active:bg-gray-100',
         ],
         'green' => [
             'solid' => 'shadow-lg shadow-green-600/50 bg-green-600 hover:bg-green-700 active:bg-green-900 text-white hover:shadow-sm focus:ring-2 focus:ring-green-500 focus:ring-offset-2',
             'outline' => 'border-2 border-green-600 text-green-600 hover:bg-green-50 active:bg-green-100 focus:ring-2 focus:ring-green-500 focus:ring-offset-2',
+            'simple' => 'text-green-600 hover:bg-green-50 active:bg-green-100',
         ],
         'red' => [
             'solid' => 'shadow-lg shadow-red-600/50 bg-red-600 hover:bg-red-700 active:bg-red-900 text-white hover:shadow-sm focus:ring-2 focus:ring-red-500 focus:ring-offset-2',
             'outline' => 'border-2 border-red-600 text-red-600 hover:bg-red-50 active:bg-red-100 focus:ring-2 focus:ring-red-500 focus:ring-offset-2',
+            'simple' => 'text-red-600 hover:bg-red-50 active:bg-red-100',
         ],
         'yellow' => [
             'solid' => 'shadow-lg shadow-yellow-500/50 bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 text-white hover:shadow-sm focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2',
             'outline' => 'border-2 border-yellow-600 text-yellow-600 hover:bg-yellow-50 active:bg-yellow-100 focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2',
+            'simple' => 'text-yellow-600 hover:bg-yellow-50 active:bg-yellow-100',
         ],
         'cyan' => [
             'solid' => 'shadow-lg shadow-cyan-600/50 bg-cyan-600 hover:bg-cyan-700 active:bg-cyan-900 text-white hover:shadow-sm focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2',
             'outline' => 'border-2 border-cyan-600 text-cyan-600 hover:bg-cyan-50 active:bg-cyan-100 focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2',
+            'simple' => 'text-cyan-600 hover:bg-cyan-50 active:bg-cyan-100',
         ],
         'teal' => [
             'solid' => 'shadow-lg shadow-teal-600/50 bg-teal-600 hover:bg-teal-700 active:bg-teal-900 text-white hover:shadow-sm focus:ring-2 focus:ring-teal-500 focus:ring-offset-2',
             'outline' => 'border-2 border-teal-600 text-teal-600 hover:bg-teal-50 active:bg-teal-100 focus:ring-2 focus:ring-teal-500 focus:ring-offset-2',
+            'simple' => 'text-teal-600 hover:bg-teal-50 active:bg-teal-100',
         ],
         'black' => [
             'solid' => 'shadow-lg shadow-gray-900/50 bg-gray-900 hover:bg-gray-900 active:bg-gray-950 text-white hover:shadow-sm focus:ring-2 focus:ring-gray-700 focus:ring-offset-2',
             'outline' => 'border-2 border-gray-900 text-gray-900 hover:bg-gray-50 active:bg-gray-100 focus:ring-2 focus:ring-gray-700 focus:ring-offset-2',
+            'simple' => 'text-gray-900 hover:bg-gray-50 active:bg-gray-100',
         ],
     ];
 
