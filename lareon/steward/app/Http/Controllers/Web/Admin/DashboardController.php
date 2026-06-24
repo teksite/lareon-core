64fdc8a8cdc4f8b4c99eafef04a3cd73af45b140
teksite <teksite.design@gmail.com>
2026-06-24 13:51:08 +0330

    make panel dashboard controller

--- lareon/steward/app/Http/Controllers/Web/Admin/DashboardController.php
+++ lareon/steward/app/Http/Controllers/Web/Admin/DashboardController.php
@@ -2,11 +2,21 @@
 
 namespace Lareon\Steward\App\Http\Controllers\Web\Admin;
 
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 use Lareon\Steward\App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class DashboardController extends Controller implements HasMiddleware
 {
+    public function __construct() {}
+
+    public static function middleware()
+    {
+        return [
+            new Middleware('can:admin'),
+        ];
+    }
     public function index()
     {
         return view("lareon::admin.pages.dashboard");
--- lareon/steward/routes/panel/web.php
+++ lareon/steward/routes/panel/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Lareon\Steward\App\Http\Controllers\Web\Admin\DashboardController;
-use Lareon\Steward\App\Http\Controllers\Web\Admin\Settings\CacheController;
-use Lareon\Steward\App\Http\Controllers\Web\Admin\Settings\LogsController;
-use Lareon\Steward\App\Http\Controllers\Web\Admin\Settings\MaintenanceModeController;
-use Lareon\Steward\App\Http\Controllers\Web\Admin\Settings\SystemInformationController;
-
+use Lareon\Steward\App\Http\Controllers\Web\Panel\DashboardController;
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
