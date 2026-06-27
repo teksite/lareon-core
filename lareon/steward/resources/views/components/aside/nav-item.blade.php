6735c2cb34b0187e173db0897172806f890846f7
teksite <teksite.design@gmail.com>
2026-06-27 17:36:35 +0330

    change aside behavior admin to click

--- lareon/steward/resources/views/components/aside/nav-item.blade.php
+++ lareon/steward/resources/views/components/aside/nav-item.blade.php
@@ -10,14 +10,14 @@
      || ($url && str_starts_with($url , request()->url())));
 @endphp
 
-<div class="group p-1">
-    <button type="button" role="button" class="group-hover:bg-slate-100 flex items-center gap-1 flex-col w-full bordering p-1 rounded-lg mx-auto {{$active ? 'bg-blue-100 text-blue-900' : ''}}">
+<div x-data="{open:false}" @click.outside="open=false" class="p-1">
+    <button @click="open=!open" type="button" role="button" class="group-hover:bg-slate-100 flex items-center gap-1 flex-col w-full bordering p-1 rounded-lg mx-auto {{$active ? 'bg-blue-100 text-blue-900' : ''}}">
         <x-icon class="stroke-current fill-none" type="{{$menu['icon-type'] ?? 'outline' }}" icon="{{$menu['icon'] ??  'circle'}}" high="{{$size}}" width="{{$size}}"/>
         <span class="text-xs font-semibold">
         {{__($menu['title'])}}
     </span>
     </button>
-    <div class="group-hover:block hidden absolute z-10 -end-48  top-1 bottom-0 h-[99%] w-48 bg-slate-50 rounded-e-lg bordering">
+    <div  x-show="open"  class="absolute z-10 -end-48  top-1 bottom-0 h-[99%] w-48 bg-slate-50 rounded-e-lg bordering">
         <span class="block text-center font-bold text-sm py-2">
             {{__($menu['title'])}}
         </span>
