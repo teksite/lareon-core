9d1dee39b2428d8c797c920f2411c8dbd7e377a0
teksite <teksite.design@gmail.com>
2026-06-28 16:45:36 +0330

    add style to indicate selected nav in admin aside

--- lareon/steward/resources/views/components/aside/nav-item.blade.php
+++ lareon/steward/resources/views/components/aside/nav-item.blade.php
@@ -10,8 +10,8 @@
      || ($url && str_starts_with($url , request()->url())));
 @endphp
 
-<div x-data="{open:false}" @click.outside="open=false" class="p-1">
-    <button @click="open=!open" type="button" role="button" class="group-hover:bg-slate-100 flex items-center gap-1 flex-col w-full bordering p-1 rounded-lg mx-auto {{$active ? 'bg-blue-100 text-blue-900' : ''}}">
+<div x-data="{open:false}" @click.outside="open=false" class="group p-1">
+    <button @click="open=!open" type="button" role="button" class="group-hover:bg-slate-100 flex items-center gap-1 flex-col w-full bordering p-1 rounded-lg mx-auto {{$active ? 'bg-blue-100 text-blue-900 border border-blue-900' : 'bordering'}}" :class="{'border border-blue-900' : open}">
         <x-icon class="stroke-current fill-none" type="{{$menu['icon-type'] ?? 'outline' }}" icon="{{$menu['icon'] ??  'circle'}}" high="{{$size}}" width="{{$size}}"/>
         <span class="text-xs font-semibold">
         {{__($menu['title'])}}
