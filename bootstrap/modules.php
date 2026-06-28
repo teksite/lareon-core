5cbcd8b2fb238e14c8706e0c3de3a939f74777f6
teksite <teksite.design@gmail.com>
2026-06-28 16:51:45 +0330

    stafrt to make FileManager module

--- bootstrap/modules.php
+++ bootstrap/modules.php
@@ -1,15 +1,17 @@
 <?php return [
-    'User' => [
-        'provider' => 'Lareon\\Modules\\User\\App\\Providers\\UserServiceProvider',
-        'active'   => true,
-        'type'     => 'steward',
-    ],
-
-    'Auth' => [
-        'provider' => 'Lareon\\Modules\\Auth\\App\\Providers\\AuthServiceProvider',
-        'active'   => true,
-        'type'     => 'steward',
-    ],
-
-
+  'User' => [
+    'provider' => 'Lareon\\Modules\\User\\App\\Providers\\UserServiceProvider',
+    'active' => true,
+    'type' => 'steward',
+  ],
+  'Auth' => [
+    'provider' => 'Lareon\\Modules\\Auth\\App\\Providers\\AuthServiceProvider',
+    'active' => true,
+    'type' => 'steward',
+  ],
+  'FileManager' => [
+    'provider' => 'Lareon\\Modules\\FileManager\\App\\Providers\\FileManagerServiceProvider',
+    'active' => true,
+    'type' => 'self',
+  ],
 ];
