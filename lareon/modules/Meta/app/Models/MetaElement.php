30e869c157b5324fe66476342b1f6aacfacb62e4
teksite <teksite.design@gmail.com>
2026-07-28 17:52:31 +0330

    make a chagge in model migration and sartot to conteollers in meta

--- lareon/modules/Meta/app/Models/MetaElement.php
+++ lareon/modules/Meta/app/Models/MetaElement.php
@@ -17,8 +17,14 @@ class MetaElement extends Model
     public static function rules(string $operation, int|null $id = null): array
     {
         return match (true) {
-            $operation === 'create'          => ['title' => 'required|string|max:100',],
-            ($operation === 'update' && $id) => ['title' => ['required', 'string', Rule::unique('meta_elements', 'title')->ignore($id)],],
+            $operation === 'create'          => [
+                'title' => 'required|string|max:100',
+                'path'  => 'required|string|max:100',
+            ],
+            ($operation === 'update' && $id) => [
+                'title' => ['required', 'string', Rule::unique('meta_elements', 'title')->ignore($id)],
+                'path'  => ['required', 'string', Rule::unique('meta_elements', 'path')->ignore($id)],
+            ],
             default                          => throw new \InvalidArgumentException("Operation '{$operation}' is not valid. Allowed: create, update")
         };
     }
