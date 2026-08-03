acf2cc27a3a74861ffd9461a85360bcea33d8645
teksite <teksite.design@gmail.com>
2026-08-03 15:58:45 +0330

    make SeoSitemap model

--- lareon/modules/Seo/app/Models/SeoSite.php
+++ lareon/modules/Seo/app/Models/SeoSite.php
@@ -2,9 +2,27 @@
 
 namespace Lareon\Modules\Seo\App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['key','value','state'])]
 class SeoSite extends Model
 {
-    //
+    protected function casts(): array
+    {
+        return [
+            'key' => 'string',
+            'value' => 'json',
+            'state' => 'boolean'
+        ];
+    }
+
+    public static function rules(): array
+    {
+        return [
+            'key'=>'required|string',
+            'value'=>'required|array',
+            'state'=>'required|boolean',
+        ];
+    }
 }
