e26f69484c658fd1fbe59121364259139c8dd34b
teksite <teksite.design@gmail.com>
2026-08-06 23:35:28 +0330

    fix error in Tempalte component in Meta

--- lareon/steward/resources/views/components/editor/section/template.blade.php
+++ lareon/steward/resources/views/components/editor/section/template.blade.php
@@ -6,16 +6,14 @@
         default => null
     };
    $templates = \Lareon\Modules\Meta\App\Models\MetaTemplate::query()->where('model_type', $type)->pluck('title' ,'id')->toArray();
+
 @endphp
 
-
 <section class="{{$wrapperClass}}">
-    <x-lareon::inputs.label for="template_selector" :title="__('template')" :required="$required" class="mb-3"/>
-    <x-lareon::inputs.select id="template_selector" class="block mt-1 w-full" name="template_id" aria-label="{{__('template selector')}}">
-        <option value="" {{old('template', $value) ===null ? 'selected' :'' }}>{{__('default')}}</option>
+    <x-lareon::editor.input-select :value="$value" :required="$required" :label="__('template')" name="template_id" aria-label="{{__('template selector')}}">
+        <option value="">{{__('default')}}</option>
         @foreach($templates as $id=>$temp)
-            <option value="{{$id}}" {{old('template', $value) === $id ? 'selected' :'' }}>{{$temp}}</option>
+            <option value="{{$id}}">{{$temp}}</option>
         @endforeach
     </x-lareon::editor.input-select>
-    <x-lareon::inputs.error :messages="$errors->get('template')" class="mt-2"/>
 </section>
