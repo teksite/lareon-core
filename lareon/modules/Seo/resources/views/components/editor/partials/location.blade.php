624cfeff1bec6860fd2856f59279f12895d44e4c
teksite <teksite.design@gmail.com>
2026-08-12 15:38:33 +0330

    refactor location-place ->event vew in Seo

--- lareon/modules/Seo/resources/views/components/editor/partials/location.blade.php
+++ lareon/modules/Seo/resources/views/components/editor/partials/location.blade.php
@@ -1,12 +1,12 @@
-@props(['name','value'=>[], 'required'=>true, 'arrayName'=>'place' ,'title'=>__('location')])
+@props(['name','value'=>[], 'required'=>true, 'arrayName'=>'location' ,'title'=>__('location')])
 @php
     $finalName = $name."[".$arrayName."]";
 @endphp
 <fieldset class="fieldset">
     <legend class="legend">{{$title}}</legend>
-    <x-lareon::editor.input :label="__('name')" name="{{$finalName}}[name]" :value="$value['name'] ?? null" labelPosition="start" :required="$required" :placeholder="__('lareon::global.placeholders.empty.read',['attribute'=>__('meta')])"/>
+    <x-lareon::editor.input :label="__('name')" name="{{$finalName}}[place][name]" :value="$value['place']['name'] ?? null" labelPosition="start" :required="$required" :placeholder="__('lareon::global.placeholders.empty.read',['attribute'=>__('meta')])"/>
 
-    <x-seo::editor.partials.address name="{{$name}}" :value="$value['address'] ?? []" :name="$finalName"/>
+    <x-seo::editor.partials.address name="{{$name}}" :value="$value['place']['address'] ?? []" :name="$finalName.'[place]'"/>
     <x-seo::editor.partials.virtual-location name="{{$name}}" :value="$value['VirtualLocation'] ?? []" :name="$finalName"/>
 
 </fieldset>
--- lareon/modules/Seo/resources/views/components/editor/types/event-page.blade.php
+++ lareon/modules/Seo/resources/views/components/editor/types/event-page.blade.php
@@ -4,7 +4,7 @@
     <legend class="legend">{{__('event page schema')}}</legend>
     <div class="space-y-6">
         <x-seo::editor.partials.event name="{{$name}}" :value="$value['event'] ?? []"/>
-        <x-seo::editor.partials.location name="{{$name}}" :value="$value['place'] ?? []"/>
+        <x-seo::editor.partials.location name="{{$name}}" :value="$value['location'] ?? []"/>
         <x-seo::editor.partials.performer name="{{$name}}" :value="$value['performer'] ?? []"/>
         <x-seo::editor.partials.offers name="{{$name}}" :value="$value['offers'] ?? []"/>
     </div>
