65830f043b7cbe3d212b2072a115cf58f3b4273c
teksite <teksite.design@gmail.com>
2026-08-12 15:47:02 +0330

    add types

--- lareon/modules/Seo/resources/views/components/editor/partials/same-as.blade.php
+++ lareon/modules/Seo/resources/views/components/editor/partials/same-as.blade.php
@@ -2,41 +2,45 @@
 
 @php
     $finalName = $name."[".$arrayName."]";
-    $dottedName = str_replace(['[', ']'], ['.', ''], $finalName);
-    $rawItems = old($dottedName, null) ?? $value ?? [];
 
-    $initialItems = collect($rawItems)->map(fn ($item) =>is_array($item)
-       ? ['url'=> $item['url'] ?? '',]
-       : ['url'  => $item,])->values();
+    $dottedName = str_replace(['[', ']'],    ['.', ''],    $finalName);
+    $rawItems = old($dottedName, $value ?? []);
+
+    $initialItems = collect($rawItems)
+        ->filter(fn ($item) => is_string($item) && trim($item) !== '')
+        ->values()
+        ->toArray();
 @endphp
-
-<fieldset class="fieldset"
-          x-data="{
-        items: {{ $initialItems->toJson() }},
-        errors: @js($errors->getMessages()),
-        addItem() { this.items.push({ url: '',}); },
-        removeItem(index) { this.items.splice(index, 1); },
-        hasError(key) { return this.errors[key] !== undefined; },
-        getError(key) { return this.errors[key]?.[0] ?? ''; }
-    }"
->
+<fieldset class="fieldset">
     <legend class="legend">{{$title}}</legend>
-    @error($dottedName)
-    <p class="mb-4 message-error">{{ $message }}</p>
-    @enderror
-
-    <div class="space-y-6">
+    <div
+        class="space-y-6"
+        x-data="{
+        items: @js($initialItems),
+        errors: @js($errors->getMessages()),
+        addItem() { this.items.push('');},
+        removeItem(index) { this.items.splice(index, 1);},
+        hasError(key) { return Object.prototype.hasOwnProperty.call(this.errors, key); },
+        getError(key) { return this.errors[key]?.[0] ?? '';}
+    }"
+    >
+        @error($dottedName)
+        <p class="mb-4 message-error">
+            {{ $message }}
+        </p>
+        @enderror
 
         <template x-for="(item, index) in items" :key="index">
             <div class="mb-6 flex items-center justify-between gap-6">
                 <div class="w-full flex flex-col gap-1">
-                    <label class="input_label" :for="`sameass_url-${index}`" x-text="`{{ __('url') }} #${index + 1}`"></label>
+                    <label class="input_label" :for="`sameas_url-${index}`" x-text="`{{ __('url') }} #${index + 1}`"></label>
+
                     <input type="text" dir="ltr" @required($required) class="input block w-full" placeholder="https://example.com"
-                           :class="{'input-error':hasError('{{ $dottedName }}.' + index + '.url')}"
-                           :name="`{{ $finalName }}[${index}][url]`"
-                           :id="`sameass_url-${index}`"
-                           x-model="item.url">
-                    <p class="message-error" x-show="hasError('{{ $dottedName }}.' + index + '.url')" x-text="getError('{{ $dottedName }}.' + index + '.url')"></p>
+                           :class="{'input-error': hasError('{{ $dottedName }}.' + index )}"
+                           :name="`{{ $finalName }}[${index}]`"
+                           :id="`sameas_url-${index}`"
+                           x-model="items[index]">
+                    <p class="message-error" x-show="hasError('{{ $dottedName }}.' + index)" x-text="getError('{{ $dottedName }}.' + index)"></p>
                 </div>
                 <x-lareon::buttons.simple size="2xs" color="red" variant="outline" type="button" role="button" title="{{ __('double click to delete') }}" @dblclick="removeItem(index)">
                     &times;
--- lareon/modules/Seo/resources/views/components/editor/types/person-page.blade.php
+++ lareon/modules/Seo/resources/views/components/editor/types/person-page.blade.php
@@ -5,7 +5,7 @@
     <div class="space-y-6">
         <x-seo::editor.partials.person name="{{$name}}" :value="$value['person'] ?? []"/>
         <x-seo::editor.partials.same-as :name="$name" :value="$value['sameAs']?? []"/>
-        <x-seo::editor.partials.organization :name="$name" :value="$value['organization']?? []"/>
+        <x-seo::editor.partials.organization :name="$name.'[worksFor]'" :value="$value['worksFor']['organization'] ?? []"  :title="__('works for')"/>
 
     </div>
 </fieldset>
