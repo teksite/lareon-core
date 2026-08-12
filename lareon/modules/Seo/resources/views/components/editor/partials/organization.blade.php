e67731f7455ff4e5df945156b5179781a0c90c85
teksite <teksite.design@gmail.com>
2026-08-12 15:16:59 +0330

    refactor publiuser(organization) vew in Seo

--- lareon/modules/Seo/resources/views/components/editor/partials/organization.blade.php
+++ lareon/modules/Seo/resources/views/components/editor/partials/organization.blade.php
@@ -1,15 +1,11 @@
-@props(['name','value'=>[], 'required'=>true ,'arrayName'=>'organization' ,'title'=>__('organization')])
+@props(['name'=>'seo[schema]','value'=>[], 'required'=>false, 'arrayName'=>'organization' ,'title'=>__('organization')])
 @php
     $finalName = $name."[".$arrayName."]";
-    $dottedName = str_replace(['[', ']'], ['.', ''], $finalName);
 @endphp
 <fieldset class="fieldset">
     <legend class="legend">{{$title}}</legend>
-    @error($dottedName)
-    <p class="mb-4 message-error">{{ $message }}</p>
-    @enderror
-    <div class="space-y-6">
-        <x-lareon::editor.input labelPosition="start" :label="__('title')" name="{{$finalName}}[organization][title]" :value="$value['organization']['title'] ?? null" :placeholder="__('lareon::global.placeholders.empty.read',['attribute'=>__('website')])"/>
-        <x-lareon::editor.input labelPosition="start" :label="__('logo')" name="{{$finalName}}[organization][logo]" :value="$value['organization']['logo'] ?? null" placeholder="https://exmaple.com//images/logo.jpg | /images/logo.jpg" dir="ltr" :required="true"/>
+    <div class="grid gap-6 md:grid-cols-2">
+        <x-lareon::editor.input :label="__('name')" name="{{$finalName}}[name]" :value="$value['name'] ?? null" labelPosition="start" :required="$required"/>
+        <x-lareon::editor.input :label="__('logo')" name="{{$finalName}}[logo]" :value="$value['logo'] ?? null" labelPosition="start" dir="ltr" :required="$required" placeholder="https://example.com/logo.png | /logo.png "/>
     </div>
 </fieldset>
--- lareon/modules/Seo/resources/views/components/editor/types/article-page.blade.php
+++ lareon/modules/Seo/resources/views/components/editor/types/article-page.blade.php
@@ -7,5 +7,5 @@
     <x-seo::editor.partials.article :name="$name" :value="$value['article']?? []"/>
     <x-seo::editor.partials.image :name="$name" :value="$value['image']?? []"/>
     <x-seo::editor.partials.author :name="$name" :value="$value['author']?? []"/>
-    <x-seo::editor.partials.publisher :name="$name" :value="$value['publisher']?? []"/>
+    <x-seo::editor.partials.organization :name="$name.'[publisher]'" :title="__('publisher')" :value="$value['publisher']['organization']?? []"/>
 </fieldset>
