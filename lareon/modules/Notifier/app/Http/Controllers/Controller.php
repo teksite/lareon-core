d950877a6cb4aea7e6ce7348b7788d3fe8ec75aa
teksite <teksite.design@gmail.com>
2026-08-19 19:16:44 +0330

    add Notifier module

---
+++ lareon/modules/Notifier/app/Http/Controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Lareon\Modules\Notifier\App\Http\Controllers;
+
+abstract class Controller
+{
+    //
+}
---
+++ lareon/modules/Notifier/database/seeders/NotifierDatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lareon\Modules\Notifier\Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NotifierDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+        //
+        ]);
+    }
+}
