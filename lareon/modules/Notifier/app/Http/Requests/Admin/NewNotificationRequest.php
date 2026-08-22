7d4f8f7a11b0d0e5667335b0c03f3ebd067ade14
teksite <teksite.design@gmail.com>
2026-08-22 21:19:54 +0330

    add NewNotificationRerquest in notifier

---
+++ lareon/modules/Notifier/app/Http/Requests/Admin/NewNotificationRequest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Lareon\Modules\Notifier\App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewNotificationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return userCan('admin.notification.create');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:100',
+            'message' => 'required|string|max:500',
+            'pinned' => 'boolean',
+        ];
+    }
+}
