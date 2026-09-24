b319ef07c3762ea8650caa7e77cd892af4afe7a4
teksite <teksite.design@gmail.com>
2026-09-24 15:21:30 +0330

    make client routes raw controller Questionnaire module

--- lareon/modules/Questionnaire/routes/ajax.php
+++ lareon/modules/Questionnaire/routes/ajax.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Lareon\Modules\Questionnaire\App\Http\Controllers\Web\Client\Inboxes\SubmitController;
+use Lareon\Modules\Questionnaire\App\Http\Controllers\Ajax\Client\Inboxes\SubmitController;
 
 Route::post('/client-submitting/form',[SubmitController::class ,'store'])->name('client.submitting.form')->middleware('throttle:5,1');
