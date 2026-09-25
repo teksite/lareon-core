25cb0e95054c345609f8df5e3faf443ed4608169
teksite <teksite.design@gmail.com>
2026-09-25 10:37:56 +0330

    add trash form in questionnaire

--- lareon/modules/Questionnaire/app/Http/Controllers/Web/Admin/Forms/TrashFormsController.php
+++ lareon/modules/Questionnaire/app/Http/Controllers/Web/Admin/Forms/TrashFormsController.php
@@ -2,51 +2,37 @@
 
 namespace Lareon\Modules\Questionnaire\App\Http\Controllers\Web\Admin\Forms;
 
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 use Lareon\Modules\Questionnaire\App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
+use Lareon\Modules\Questionnaire\App\Logics\FormLogic;
+use Lareon\Steward\App\Traits\UseTrashController;
 
-class TrashFormsController extends Controller
+class TrashFormsController extends Controller implements HasMiddleware
 {
+    use UseTrashController;
 
-    /**
-     * Display  a list of the resource.
-     */
-    public function index(): \Illuminate\Contracts\View\View
+    public string $attribute = 'form';
+
+    public ?string $view = null;
+    public string $backTo = 'admin.questionnaire.inboxes.trash.index';
+
+    public string $indexRoute = 'admin.questionnaire.inboxes.trash.index';
+    public string $pruneRoute = 'admin.questionnaire.inboxes.trash.prune';
+    public string $reinstateRoute = 'admin.questionnaire.inboxes.trash.reinstate';
+    public string $flushRoute = 'admin.questionnaire.inboxes.trash.flush';
+    public string $restoreRoute = 'admin.questionnaire.inboxes.trash.restore';
+
+
+    public function __construct(public FormLogic $logic) {}
+
+    public static function middleware(): array
     {
-        //
+        return [
+            new Middleware('can:admin.questionnaire.form.delete'),
+            new Middleware('can:admin.questionnaire.form.trash', only: ['prune', 'flush']),
+        ];
     }
-
-    /**
-     * restore one instance from trash
-     */
-    public function reinstate($id): RedirectResponse
-    {
-        //
-    }
-
-    /**
-     * delete one instance from DB forever
-     */
-    public function prune($id): RedirectResponse
-    {
-      //;
-    }
-
-    /**
-     *  restore all instances.
-     */
-    public function restore(): RedirectResponse
-    {
-       //
-    }
-
-    /**
-     * delete all instances forever.
-     */
-    public function flush(): RedirectResponse
-    {
-        //
-    }
-
 
 }
