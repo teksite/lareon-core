e6d91f18e56661c79cbaf48e162be3a91ade424a
teksite <teksite.design@gmail.com>
2026-09-26 16:56:53 +0330

    fix bug in analytics view Questionnaire

--- lareon/modules/Questionnaire/resources/views/admin/pages/analytics/index.blade.php
+++ lareon/modules/Questionnaire/resources/views/admin/pages/analytics/index.blade.php
@@ -3,18 +3,22 @@
     @section('description', __('analytics help provide a comprehensive overview at a glance'))
 
     <form action="{{ route('admin.questionnaire.analytics.show') }}" method="GET" id="inboxLineChartForm">
-        <x-lareon::box class="flex items-center gap-3 mb-12">
-            <div>
-                <x-lareon::editor.input-date :label="__('from date')" id="fromDate" name="fromDate" type="date" :value="request()->fromDate ?? now()->startOfMonth()->format('Y-m-d')"/>
-                <x-lareon::editor.input-date :label="__('until date')" id="toDate" name="toDate" type="date" :value="request()->toDate ?? now()->endOfMonth()->format('Y-m-d')"/>
-                <x-lareon::editor.input-select id="range" name="range" :label="__('range')">
-                    <option value="month">{{ __('month') }}</option>
-                    <option value="year">{{ __('year') }}</option>
-                    <option value="week">{{ __('week') }}</option>
-                    <option value="day">{{ __('day') }}</option>
-                </x-lareon::editor.input-select>
+        <x-lareon::box class="flex items-end gap-3 mb-12">
+            <div class="flex gap-1">
+                <div class="flex gap-1">
+                    <x-lareon::editor.input-date :label="__('from date')" id="fromDate" name="fromDate" type="date" :value="request()->fromDate ?? now()->startOfMonth()->format('Y-m-d')"/>
+                    <x-lareon::editor.input-date :label="__('until date')" id="toDate" name="toDate" type="date" :value="request()->toDate ?? now()->endOfMonth()->format('Y-m-d')"/>
+                </div>
+                <div class="flex gap-1">
+                    <x-lareon::editor.input-select id="range" name="range" :label="__('range')">
+                        <option value="month">{{ __('month') }}</option>
+                        <option value="year">{{ __('year') }}</option>
+                        <option value="week">{{ __('week') }}</option>
+                        <option value="day">{{ __('day') }}</option>
+                    </x-lareon::editor.input-select>
+                </div>
             </div>
-            <x-lareon::buttons.nav class="min-w-36" :fullWidth="false" type="submit" color="blue" >
+            <x-lareon::buttons.nav class="min-w-24" :fullWidth="false" type="submit" color="blue">
                 {{ 'submit' }}
             </x-lareon::buttons.nav>
         </x-lareon::box>
